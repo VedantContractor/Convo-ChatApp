141471d300bde04801f64e958bf3215246b07fa2
Vedant Contractor <vedantcontractor11@gmail.com>
2026-07-29 18:41:18 +0530

    commit

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -16,7 +16,7 @@ const Signup = () => {
     e.preventDefault();
     try {
       const res = await axios.post(
-        "`${import.meta.env.VITE_API_URL}`/api/v1/user/register",
+        `${import.meta.env.VITE_API_URL}/api/v1/user/register`,
         user,
         {
           headers: {
